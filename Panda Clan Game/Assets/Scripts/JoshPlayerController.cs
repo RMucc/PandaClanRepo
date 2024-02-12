1ae807b2860a39d725aeae5aa20311885b603953
JoshuaSanders95 <126491082+JoshuaSanders95@users.noreply.github.com>
2024-02-12 17:33:58 -0500

    General Update

--- Panda Clan Game/Assets/Scripts/JoshPlayerController.cs
+++ Panda Clan Game/Assets/Scripts/JoshPlayerController.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 //This is a script for my own player I can use to test in my scene. I will make it a PreFab so that other's
 // can use this player if they so choose
-//Hello
 public class JoshPlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
